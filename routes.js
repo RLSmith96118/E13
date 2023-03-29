dbfbc4e929e7f95535df371cf37f71f88ea6740f
Matt Ball <matt.ball@cgi.com>
2023-03-29 16:43:44 +0100

    about page added and root

--- routes.js
+++ routes.js
@@ -2,12 +2,16 @@ var express = require("express");
 
 var router = express.Router();
 
-router.get("/", function(req,res){
+router.get("/", function (req, res) {
    res.render("index");
 });
 
-router.get("/home", function(req,res){
-    res.render("home");
- });
+router.get("/home", function (req, res) {
+   res.render("home");
+});
+
+router.get("/about", function (req, res) {
+   res.render("about");
+});
 
 module.exports = router;
